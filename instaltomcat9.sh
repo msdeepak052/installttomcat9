13a2e7805c59c01a99c41a8ec9810ff5e973811d
msdeepak052 <160315954+msdeepak052@users.noreply.github.com>
2024-10-11 16:02:30 +0530

    Update instaltomcat9.sh

--- instaltomcat9.sh
+++ instaltomcat9.sh
@@ -3,10 +3,10 @@ sudo yum install java-1.8.* -y
 cd /opt
 sudo yum install wget -y
 sudo yum install net-tools -y 
-sudo wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.95/bin/apache-tomcat-9.0.95.tar.gz
-sudo tar -xvf apache-tomcat-9.0.95.tar.gz
-sudo rm apache-tomcat-9.0.95.tar.gz
-sudo mv apache-tomcat-9.0.95 tomcat9
+sudo wget https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.96/bin/apache-tomcat-9.0.96.tar.gz
+sudo tar -xvf apache-tomcat-9.0.96.tar.gz
+sudo rm apache-tomcat-9.0.96.tar.gz
+sudo mv apache-tomcat-9.0.96 tomcat9
 sudo chmod 777 -R tomcat9
 cd /opt/tomcat9
 ./bin/startup.sh
